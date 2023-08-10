72e2d841dc27816391ce041ed32bc5c0bd9b857a
gmmorgan1996 <griffin.mrogan@gmail.com>
2023-08-10 18:44:16 -0400

    test commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gmmorgan96/test
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gmmorgan96/test/server"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	server.Hello()
+}
